c2465396aa3f8f334179782058c5de7573c5eb82
oriioll <oriolplazas2@gmail.com>
2025-11-12 16:55:47 +0100

    css v1, titles, buttons, containers and responsive

---
+++ src/scripts/scripts.js
@@ -0,0 +1,13 @@
+/*
+TO DO:
+- Change timer color to workmode/breakmode depending on its status
+- Change svg  start pause color the same
+- Create timer 25:00 mins to 0 and when 
+  its 0 switch to break mode 5:00 to 0 and then 25 again
+- Boolean variable (workmode) if true, change colors and timer to 25
+  and if false timer to 5 and change colors again
+- Start/Pause and reset buttons functionality
+- Work and break mode buttons 
+  (just check if clicked and if so change variable)
+- Sound when finishing workmode/breakmode
+*/
